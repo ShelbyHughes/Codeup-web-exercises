618bee66f0a960f914d72f0d754e0379e95daacc
Shelby Hughes <shelbyhughes2019@gmail.com>
2019-11-26 09:34:14 -0600

    Completed warm up for 11/26/19

---
+++ exercise3and4.js
@@ -0,0 +1,8 @@
+var pricePerDay = 3;
+var littleMermaid = 3;
+var brotherBear = 5;
+var hercules = 1;
+
+hercules * pricePerDay = hercules;
+brotherBear * pricePerDay = brotherBear;
+littleMermaid * pricePerDay = littleMermaid;
