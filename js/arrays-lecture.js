ca64487e61f39c00c9c71f6f146e4d98d3dbe3e7
Shelby Hughes <shelbyhughes2019@gmail.com>
2019-12-09 14:57:34 -0600

    Arrays lecture files

---
+++ js/arrays-lecture.js
@@ -0,0 +1,54 @@
+"use strict";
+// var pet1 = "Toby";
+// var pet2 = "Princess";
+// var pet3 = "Samson";
+// var pet4 = "Jill";
+// var pet5 = "Bubbles";
+// var pet6 = "Malu";
+
+// var pets = [];
+//     pets[0] = "Toby";
+//     pets[1] = "Princess";
+//     pets[2] = "Samson";
+//     pets[3] = "Jill";
+//     pets[4] = "Bubbles";
+//     pets[5] = "Malu";
+
+var pets = ["Toby", "Princess", "Samson", "Jill", "Bubbles", "Malu"];
+
+for(var i = 0; i<pets.length; i++){
+    console.log(pets[i]);
+}
+
+// ^ Equals v
+
+var l = 0;
+while(l<pets.length){
+    console.log(pets[l]);
+    l++;
+}
+
+pets[pets.length] = "Lily";
+console.log(pets[pets.length-1]);
+
+// pets[100] = "Roxeanne";
+
+pets.forEach(function(pet, i, arr){
+    console.log(pet);
+    // console.log(arr[i]);
+});
+
+// i   log      pets.length
+// 0   Toby     7
+// 1   Princess
+// 2   Samson
+// 3   Jill
+// 4   Bubbles
+// 5   Malu
+// 6   Lily
+
+var petNames = ["Princess", "Delilah", "Jephery", "Johnathan", "Pablo", "Enrique", "Keanu", "Twilight", "Turtle 1", "Turtle 2", "Turtle 3"];
+
+for(var j = 0;j<petNames.length;j++){
+    console.log(petNames[j]);
+}
