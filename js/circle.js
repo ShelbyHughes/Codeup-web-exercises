3da91846c15754de9fe57d2928d271b1b300d6bf
Shelby Hughes <shelbyhughes2019@gmail.com>
2019-12-12 11:18:23 -0600

    Commiting

--- js/circle.js
+++ js/circle.js
@@ -8,21 +8,21 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-            var area = Math.PI * circle.radius ** 2;
+            var area = Math.PI * Math.pow(this.radius, 2);
             return area; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
-            if(doRounding == true){
-                console.log(Math.round(doRounding));
+            if(doRounding === true){
+                console.log(Math.round(this.getArea()));
             }else{
-                console.log(doRounding);
+                console.log(this.getArea());
             }
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + this.getArea() );
         }
     };
 
@@ -34,6 +34,8 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
